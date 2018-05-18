392721b781306b4a99f2c2c290ce7cfb408e6bd3
Vitali Budzko <labanka1983@gmail.com>
2018-05-18 23:30:52 +0300

    Five buttons have been created.

--- food tracker/RatingControl.swift
+++ food tracker/RatingControl.swift
@@ -8,9 +8,31 @@
 
 import UIKit
 
-class RaitingControl: UIStackView {
+@IBDesignable class RaitingControl: UIStackView {
+    
+    // MARK: Properties
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    private var ratingButton = [UIButton]()
+    
+    var rating = 0
     
     // MARK: Initialization
+    // MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupButtons()
@@ -22,16 +44,29 @@ class RaitingControl: UIStackView {
     // MARK: Private Methods
     private func setupButtons() {
         
+        for _ in 0..<starCount {
         // Create the button
         let button = UIButton()
         button.backgroundColor = UIColor.red
         
         // Add constrains
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+        button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RaitingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         
         // Add the button to the stack
         addArrangedSubview(button)
+            
+        // Add the new button to the rating button array
+        ratingButton.append(button)
     }
 }
+}
+
+
+
+
+
